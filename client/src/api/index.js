3a2a91bfb89278a160d8c8fc7a649433f0a8b31d
Lisa Ma <lisa@yonomi.co>
2019-09-28 10:11:48 -0600

    Add basic fetch

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,18 @@
+import axios  from 'axios'
+const apiEndpoint = process.env.NODE_ENV === 'development' ? 'http://localhost:3001/api': ''
+const createAPIRoute = (endpoint) => {
+    return `${apiEndpoint}/${endpoint}`
+}
 export const api = {
     fetchReports: async (request) => {
-
-    }
+        await axios({
+            method: 'post',
+            url: createAPIRoute('reports'),
+            data: {
+                latitude: 40.018850,
+                longitude: -105.264510,
+                radiusInMiles: 5
+            }
+        })
+    },
 }
